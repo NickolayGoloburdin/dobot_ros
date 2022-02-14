667e8f774987e76a1be60c2068632b1203ad80e6
Nickolaygoloburdin <Nickolaygoloburdin@mail.ru>
2022-02-14 15:45:44 +0300

    add src

---
+++ src/src/dobot/src/SetPointUSB.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+from serial.tools import list_ports
+import time
+from pydobot import Dobot
+import rospy
+import math as m
+from dobot.srv import NotePoint
+from geometry_msgs.msg import Point
+pub = rospy.Publisher('/Point',Point, queue_size=10)
+port = list_ports.comports()[0].device
+device = Dobot(port=port, verbose=False)
+l1 = 138
+l2 = 0
+l3= 135.013
+l4 = 147.085
+l5 = 31
+l6 = 13
+def ParseMsg(msg):
+    try:
+        coord_list = msg.point.split()
+        x = float(coord_list[0])
+        y = float(coord_list[1])
+        z = float(coord_list[2])
+        return x,y,z
+        except ValueError:
+        rospy.logerr('Input Error')
+def SetPoint(data):
+    X,Y,Z = ParseMsg(data)
+    if X == 0:
+        if Y > 0:
+            alpha1 = m.pi/2
+        if Y < 0:
+            alpha1 = -m.pi/2
+    else:
+        alpha1 = m.atan2(Y,X)
+
+    x = X -(l2 + l5)*m.cos(alpha1)
+    y = Y -(l2 + l5)*m.sin(alpha1)
+    z = Z + l6 -l1
+    k = m.sqrt(m.pow(x,2)+m.pow(y,2))
+    d = m.sqrt(m.pow(k,2)+m.pow(z,2))
+    if (k!=0 and d!=0):
+        alpha2 = m.pi/2 - m.atan2(z,k) - m.acos((m.pow(d,2)+m.pow(l3,2)-m.pow(l4,2))/(2*d*l3))
+        alpha3 = (m.pi - m.acos((m.pow(l3,2)+m.pow(l4,2)-m.pow(d,2))/(2*l4*l3))) - alpha2 
+        if ((alpha1 < 2.108 and (alpha1 > -2.395) and (alpha2 > -0.184) and (alpha2 < 2) and (alpha3 > -0.3403392) and (alpha3 < 2))):
+            msg = Point()
+            msg.x = alpha1
+            msg.y = alpha2
+            msg.z = alpha3
+            pub.publish(msg)
+            device.move_to(X, Y, Z)
+            time.sleep(2)
+
+            return True
+        else:
+            return False
+            
+           
+
+rospy.init_node('SetPoint')
+
+service=rospy.Service('SetPoint',NotePoint,SetPoint)
+
+rospy.spin()
